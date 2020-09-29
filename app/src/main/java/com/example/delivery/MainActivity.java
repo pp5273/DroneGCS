27d22c3879b044dbb344d7cfb3d3dbaaeb5a6d68
pp5273 <pp5273@naver.com>
2020-09-29 16:50:02 +0900

    database request error

--- app/src/main/java/com/example/delivery/MainActivity.java
+++ app/src/main/java/com/example/delivery/MainActivity.java
@@ -9,9 +9,11 @@ import androidx.fragment.app.FragmentManager;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import android.os.Handler;
@@ -42,28 +44,36 @@ import com.naver.maps.map.UiSettings;
 import com.naver.maps.map.overlay.Marker;
 
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     ArrayList<listdata> manuList;
     private listdata listdata;
- //   public payment2 payment2;
+    //   public payment2 payment2;
     NaverMap mNaverMap;
-   private WebView webView;
-   TextView result;
-String getAddress;
+    private WebView webView;
+    TextView result;
+    String getAddress,getpostcode,getrequest;
+
     private Handler handler;
     private Marker marker = new Marker();
-
+    private static String IP_ADDRESS = "61.33.158.137";
+    private static String TAG = "phptest";
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
- //       result = (TextView) findViewById(R.id.address);
+        //       result = (TextView) findViewById(R.id.address);
 
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.manudata();
@@ -71,8 +81,8 @@ String getAddress;
                 new NaverMapSdk.NaverCloudPlatformClient("276gbey63g"));
 
 
-        ListView listView = (ListView)findViewById(R.id.listView);
-        final Adapter myAdapter = new Adapter(this,manuList);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        final Adapter myAdapter = new Adapter(this, manuList);
         listView.setAdapter(myAdapter);
 
 
@@ -82,27 +92,28 @@ String getAddress;
                 mapset();
                 setContentView(R.layout.payment);
 
-           //     result = (TextView) findViewById(R.id.address);
-          //      result.setText(String.format("주소를 입력해 주세요."));
-      //          postcode = (TextView) findViewById(R.id.postcode);
+                //     result = (TextView) findViewById(R.id.address);
+                //      result.setText(String.format("주소를 입력해 주세요."));
+                //          postcode = (TextView) findViewById(R.id.postcode);
             }
         });
 
 
         // 핸들러를 통한 JavaScript 이벤트 반응
-     //   handler = new Handler();
+        //   handler = new Handler();
 
 
     }
-public void mapset(){
-    FragmentManager fm = getSupportFragmentManager();
-    MapFragment mapFragment = (MapFragment)fm.findFragmentById(R.id.map);
-    if (mapFragment == null) {
-        mapFragment = MapFragment.newInstance();
-        fm.beginTransaction().add(R.id.map, mapFragment).commit();
+
+    public void mapset() {
+        FragmentManager fm = getSupportFragmentManager();
+        MapFragment mapFragment = (MapFragment) fm.findFragmentById(R.id.map);
+        if (mapFragment == null) {
+            mapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mapFragment).commit();
+        }
+        mapFragment.getMapAsync(this);
     }
-    mapFragment.getMapAsync(this);
-}
 
 
     @UiThread
@@ -120,11 +131,6 @@ public void mapset(){
 
 
     }
-
-
-
-
-
 
 
 //    @SuppressLint("SetJavaScriptEnabled")
@@ -195,13 +201,10 @@ public void mapset(){
 //    }
 
 
-
-    public  void manudata(){
-    manuList= new ArrayList<listdata>();
-    manuList.add(new listdata(R.drawable.am,"아이스 아메리카노", "2000원"));
-}
-
-
+    public void manudata() {
+        manuList = new ArrayList<listdata>();
+        manuList.add(new listdata(R.drawable.am, "아이스 아메리카노", "2000원"));
+    }
 
 
     public void onBtnSetOrderTap(View view) {
@@ -213,7 +216,6 @@ public void mapset(){
 
         final AlertDialog alertDialog = builder.create();
         final Geocoder geocoder = new Geocoder(this);
-
 
 
         TextView title = dialogView.findViewById(R.id.title);
@@ -230,10 +232,10 @@ public void mapset(){
                 String address = editText.getText().toString();
 
                 try {
-                    list = geocoder.getFromLocationName(address,50);
+                    list = geocoder.getFromLocationName(address, 50);
                 } catch (IOException e) {
                     e.printStackTrace();
-                    Log.e("test","입출력 오류 - 서버에서 주소변환시 에러발생.");
+                    Log.e("test", "입출력 오류 - 서버에서 주소변환시 에러발생.");
                 }
 
                 if (list != null) {
@@ -248,7 +250,8 @@ public void mapset(){
                     }
                 }
                 alertDialog.dismiss();
-                getAddress= address;
+                getAddress = address;
+                getpostcode = (String) postcode.getText();
 
             }
         });
@@ -265,19 +268,18 @@ public void mapset(){
     }
 
 
-    public void lastordercheck(View view){
-      //  final Button ordercheck = (Button) findViewById(R.id.checkedorder);
+    public void lastordercheck(View view) {
+        //  final Button ordercheck = (Button) findViewById(R.id.checkedorder);
         setContentView(R.layout.lastordercheck);
-       // final TextView checkedaddress = findViewById(R.id.checkedaddress);
+        // final TextView checkedaddress = findViewById(R.id.checkedaddress);
         TextView checkedaddress = (TextView) findViewById(R.id.checkedaddress);
         checkedaddress.setText(getAddress);
 
 
-
     }
 
-public void returnaddress(View view){
-       // setContentView(R.layout.payment);
+    public void returnaddress(View view) {
+        // setContentView(R.layout.payment);
 //    Intent sendIntent = new Intent();
 //
 //    sendIntent.setAction(Intent.ACTION_SEND);
@@ -287,11 +289,142 @@ public void returnaddress(View view){
 //    Intent shareIntent = Intent.createChooser(sendIntent, null);
 //    startActivity(shareIntent);
 
-    Intent intent = new Intent();
-    intent.setClassName("com.example.dronedelivery", "com.example.dronedelivery.MainActivity");
-    intent.putExtra("putData", "UserData");
-    startActivityForResult(intent, 0);
 
-}
 
+    }
+
+    public  void upload(View view){
+        Button buttonInsert = (Button)findViewById(R.id.upload);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            String request="ㅁㄴㅇㅁㄴㅇㅁㄴㅇㄴㅁ";
+
+                InsertData task = new InsertData();
+                task.execute("http://" + IP_ADDRESS + "/insert.php",getAddress,getpostcode,getrequest);
+
+
+
+            }
+        });
+    }
+
+
+    class InsertData extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+        String TAG;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Please Wait", null, true, true);
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+        //    mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+
+        @Override
+        protected String doInBackground(String... params) {
+           final EditText editText = (EditText)findViewById(R.id.checkedrequest);
+            getrequest = editText.getText().toString();
+            String address = (String) params[1];
+            String request = (String) params[2];
+            String postcode = (String) params[3];
+
+
+            // 1. PHP 파일을 실행시킬 수 있는 주소와 전송할 데이터를 준비합니다.
+
+            // POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않습니다.
+            String serverURL = (String) params[0];
+
+
+            // HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 합니다.
+
+            // 전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
+
+            // 여기에 적어준 이름을 나중에 PHP에서 사용하여 값을 얻게 됩니다.
+
+            String postParameters = "&postcode=" + postcode + "&address=" + address + "&request=" + request;
+
+
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000); //5초안에 응답이 오지 않으면 예외가 발생합니다.
+
+                httpURLConnection.setConnectTimeout(5000); //5초안에 연결이 안되면 예외가 발생합니다.
+
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8")); //전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
+
+                outputStream.flush();
+                outputStream.close();
+
+
+                // 3. 응답을 읽습니다.
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+
+                    // 에러 발생
+
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                // 4. StringBuilder를 사용하여 수신되는 데이터를 저장합니다.
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+
+
+                bufferedReader.close();
+
+
+                // 5. 저장된 데이터를 스트링으로 변환하여 리턴합니다.
+                return sb.toString();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+
+        }
+
+
+    }
 }
